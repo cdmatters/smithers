2589057d6a03d23dcd1f36108387eb56bf9b2d26
Eric Hambro <eric.hambro@gmail.com>
2016-08-18 22:15:02 +0100

    remove deleted functions from headers

--- src/smithers.h
+++ src/smithers.h
@@ -25,7 +25,6 @@ class Smithers{
         Smithers();
 
         void await_registered_players(int max_players, int max_chips );
-        void play_game(int min_raise);
         void play_tournament(int chips, int min_raise, int hands_before_blind_double);
         
         void publish_to_all(const std::string& message);
@@ -33,19 +32,9 @@ class Smithers{
 
         void print_players();
 
-        std::string create_new_game_message();
-
     private:
 
         Json::Value listen_and_pull_from_queue(const std::string& player_name);
-
-
-        int assign_seats(int dealer_seat);
-        void reset_and_move_dealer_to_next_player();
-        int get_pot_value_for_game();
-
-        std::vector<Result_t> award_winnings(const std::vector<ScoredFiveCardsPair_t>& scored_hands);
-
 
         std::vector<Player> m_players;
         zmq::context_t m_zmq_context;
